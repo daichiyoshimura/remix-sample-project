b4a0f1c2d5c656fac2932ce079d6841ba2510e15
sakekasu <flatsdy0819@gmail.com>
2024-03-29 00:49:55 +0900

    [update] style

--- app/features/Rooms/RoomCard.tsx
+++ app/features/Rooms/RoomCard.tsx
@@ -15,8 +15,8 @@ const RoomCard: React.FC<RoomCardProps> = ({
 	createdBy,
 }) => {
 	return (
-		<div className="max-w-full bg-darkslategray rounded overflow-hidden shadow-md flex room-container">
-			<div className="flex-1 flex flex-col justify-between p-6 room-details text-white">
+		<div className="max-w-full bg-darkslategray rounded overflow-hidden shadow-md flex room-container p-4">
+			<div className="flex-1 flex flex-col justify-between pr-2 pl-2 room-details text-white">
 				<div className="font-bold text-xl mb-2 room-name">{name}</div>
 				<div className="text-sm mb-2 room-info">
 					Created at: {createdAt}
@@ -25,7 +25,7 @@ const RoomCard: React.FC<RoomCardProps> = ({
 					Created by: {createdBy}
 				</div>
 			</div>
-			<div className="flex items-center room-actions">
+			<div className="flex items-center room-actions pr-2 pl-2">
 				<LinkButton to={`/rooms/${id}`}>Enter</LinkButton>
 			</div>
 		</div>
