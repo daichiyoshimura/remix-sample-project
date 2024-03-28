4d456dcb87fd8230f7e175644aea5378d66a352b
sakekasu <flatsdy0819@gmail.com>
2024-03-28 18:27:45 +0900

    [add] test

---
+++ app/components/Header/Header.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+describe('Header', () => {
+	it('renders title correctly', async () => {
+		const { getByText } = render(<Header currentPageTitle={'Hello'} />);
+		const cardContent = getByText('Hello');
+		expect(cardContent).toBeDefined();
+	});
+});
