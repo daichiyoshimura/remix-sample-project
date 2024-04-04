265cf0db27cc8c8007873ac84babecd7b0b31224
sakekasu <flatsdy0819@gmail.com>
2024-04-04 13:24:26 +0900

    [update] console.log

--- app/actions/roomsAction.server.ts
+++ app/actions/roomsAction.server.ts
@@ -1,8 +1,8 @@
 import { ActionFunction, ActionFunctionArgs, TypedResponse, json } from '@remix-run/node';
 import { Room, RoomAttributes, postRoom } from '@apis/room.server';
+import { MappedTypes } from '@apis/util/mappedTypes.server';
 import { invalidMethodAction } from './invalidMethodAction.server';
 import { Message } from './message.server';
-import { MappedTypes } from '@apis/util/mappedTypes.server';
 
 export const roomsAction: ActionFunction = async (args: ActionFunctionArgs) => {
 	switch (args.request.method) {
@@ -28,7 +28,11 @@ const postRoomsAction: ActionFunction = async (
 		const accountId: string = typeof params.accountId === 'string' ? params.accountId : '';
 		const body: RoomActionRequest = await request.json();
 		const room = await postRoom({ accountId: accountId, roomAttributes: body });
-		console.log(`path:/rooms method:${request.method} request:${body} response:${room}`);
+		console.log(
+			`path:/rooms method:${request.method} request:${JSON.stringify(
+				body,
+			)} response:${JSON.stringify(room)}`,
+		);
 		return json({ room: room }, 200);
 	} catch (error) {
 		const err = error instanceof Error ? error : new Error('unexpected error');
