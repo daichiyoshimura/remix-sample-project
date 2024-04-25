e8fad7890e1afd9303006950b61b6c35bdf3ccdf
sakekasu <flatsdy0819@gmail.com>
2024-04-25 17:28:56 +0900

    remove useBinaryHooks

--- app/hooks/useBinaryState.ts
+++
@@ -1,7 +0,0 @@
-import { useReducer } from 'react';
-
-export const useBinaryState = (init: boolean): [boolean, React.DispatchWithoutAction] => {
-	return useReducer((state: boolean) => {
-		return !state;
-	}, init);
-};
