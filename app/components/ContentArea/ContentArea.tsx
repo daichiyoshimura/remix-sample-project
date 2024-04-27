52c39016a1fb90af7c592a4f10709bdb851c7676
sakekasu <flatsdy0819@gmail.com>
2024-04-27 17:32:28 +0900

    move fadeAnimation

--- app/components/ContentArea/ContentArea.tsx
+++ app/components/ContentArea/ContentArea.tsx
@@ -1,7 +1,14 @@
+import { useFadeAnimation } from '@hooks';
+
 export type ContentAreaProps = {
 	children?: React.ReactNode;
 };
 
 export const ContentArea = ({ children }: ContentAreaProps) => {
-	return <div className="relative top-12 bottom-12 pl-32 h-full overflow-y-auto">{children}</div>;
+	const fadeClass = useFadeAnimation();
+	return (
+		<div className={`relative top-12 bottom-12 pl-32 h-full overflow-y-auto ${fadeClass()}`}>
+			{children}
+		</div>
+	);
 };
--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,8 @@
 import '@tailwind.css';
 import { Links, Meta, Outlet, Scripts, ScrollRestoration, useNavigation } from '@remix-run/react';
-import { useFadeAnimation } from '@hooks';
 import { ContentArea, Footer, Header, Menu } from '@components';
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	const fadeClassName = useFadeAnimation();
 	return (
 		<html lang="en">
 			<head>
@@ -14,9 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<body>
 				<Header />
 				<Menu />
-				<div className={fadeClassName()}>
-					<ContentArea>{children}</ContentArea>
-				</div>
+				<ContentArea>{children}</ContentArea>
 				<Footer />
 				<ScrollRestoration />
 				<Scripts />
