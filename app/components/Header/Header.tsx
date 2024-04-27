287a00cc632536356ccb3e33b186074810a1c9b8
sakekasu <flatsdy0819@gmail.com>
2024-04-27 14:18:06 +0900

    add fake sign icon

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,13 @@
+import { AccountIcon } from '@components';
+
 export const Header = () => {
 	return (
-		<header className="fixed top-0 left-0 w-full h-12 bg-primary text-white p-4 flex items-center">
+		<header className="fixed top-0 left-0 w-full h-12 bg-primary text-white p-4 flex justify-between items-center">
 			<h1 className="text-2xl font-bold">Awsome App</h1>
+			<div className="flex justify-between items-center">
+				<AccountIcon />
+				<p className="px-2">{`john`}</p>
+			</div>
 		</header>
 	);
 };
