4b0e748934a840afcd265d390f9330eb0103cffb
sakekasu <flatsdy0819@gmail.com>
2024-04-30 20:12:41 +0900

    remove EditRoomModalComponent

--- app/features/index.tsx
+++ app/features/index.tsx
@@ -1,7 +1,6 @@
 export * from '@features/Participants/ParticipantGridItem';
 export * from '@features/Participants/ParticipantName';
 export * from '@features/Participants/ParticipantNameList';
-export * from '@features/Rooms/EditRoomModal';
 export * from '@features/Rooms/DeleteRoomModal';
 export * from '@features/Rooms/RoomCard';
 export * from '@features/Rooms/RoomCardList';
