7559746f8d709d56ea29681231bd231dc9d2a967
sakekasu <flatsdy0819@gmail.com>
2024-04-30 20:58:47 +0900

    remove ;

--- app/components/Layouts/ModalFormLayout.tsx
+++ app/components/Layouts/ModalFormLayout.tsx
@@ -9,7 +9,7 @@ export const ModalFormLayout = ({ inputs, buttons }: ModalFormLayoutProps) => {
 	return (
 		<div className={`w-full flex flex-col justify-start gap-y-4`}>
 			{inputs}
-			<div className={`w-full flex justify-end gap-x-4`}>{buttons}</div>;
+			<div className={`w-full flex justify-end gap-x-4`}>{buttons}</div>
 		</div>
 	);
 };
