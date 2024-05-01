8352a84dca9ee344fe3f26d8dbbeed9ab919d897
sakekasu <flatsdy0819@gmail.com>
2024-05-01 12:38:16 +0900

    adjust header & footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 export const Footer = () => {
 	return (
-		<footer className="fixed bottom-0 left-0 w-44 h-12 bg-primary text-white p-2 text-center">
-			<p className="text-xs px-2">&copy; Awsome Inc 2024 All Rights Reserved</p>
+		<footer className="fixed bottom-0 left-0 w-44 h-12 z-10 bg-primary text-white p-2 text-center">
+			<p className="text-xs">&copy; Awsome Inc 2024</p>
+			<p className="text-xs">All Rights Reserved</p>
 		</footer>
 	);
 };
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
-		<header className="fixed top-0 left-0 w-44 h-12 bg-primary text-white px-4 py-2 items-center z-10">
-			<h1 className="text-xl font-bold">Awsome App</h1>
+		<header className="fixed top-0 left-0 w-44 h-12 z-10 bg-primary text-white flex flex-col justify-center">
+			<h1 className="text-xl font-bold self-center">Awsome App</h1>
 		</header>
 	);
 };
