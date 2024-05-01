f80cafe763af4b8f099ceef9eea2b7cdb024120b
sakekasu <flatsdy0819@gmail.com>
2024-05-01 14:27:36 +0900

    delete LocationBar component

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -31,7 +31,6 @@ export * from '@components/Layouts/ModalFormLayout';
 export * from '@components/Layouts/NavigationBarLayout';
 export * from '@components/List/Divider';
 export * from '@components/List/List';
-export * from '@components/LocationBar/LocationBar';
 export * from '@components/SideBar/SideBar';
 export * from '@components/Text/DescriptionText';
 export * from '@components/Text/TitleText';
