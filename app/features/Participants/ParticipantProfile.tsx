fe8504e550395b1ae3191bc47fd2b5a725a2e1a8
sakekasu <flatsdy0819@gmail.com>
2024-05-01 13:42:56 +0900

    refactor ParticipantProfile

--- app/features/Participants/ParticipantProfile.tsx
+++ app/features/Participants/ParticipantProfile.tsx
@@ -1,4 +1,4 @@
-import { FlexStart, ImageBox } from '@components';
+import { ImageBox } from '@components';
 
 export type ParticipantProfileProps = {
 	id: string;
@@ -8,13 +8,13 @@ export type ParticipantProfileProps = {
 
 export const ParticipantProfile = ({ id, name, createdAt }: ParticipantProfileProps) => {
 	return (
-		<FlexStart>
+		<div className={`w-full flex justify-start align-middle gap-x-4 fixed`}>
 			<ImageBox url={''} />
 			<div className="flex flex-col w-full">
 				<h2 className="text-lg font-semibold">{name}</h2>
 				<p>ID: {id}</p>
 				<p>Created At: {createdAt}</p>
 			</div>
-		</FlexStart>
+		</div>
 	);
 };
--- app/routes/participants.$id.tsx
+++ app/routes/participants.$id.tsx
@@ -1,12 +1,7 @@
-import { VerticalFlexStart } from '@components';
 import { ParticipantProfile } from '@features/Participants/ParticipantProfile';
 
 const ParticipantProfilePage = () => {
-	return (
-		<VerticalFlexStart className={'fixed'}>
-			<ParticipantProfile id={'qwerty'} name={'Jimmy'} createdAt={'2024-04-29 16:44'} />
-		</VerticalFlexStart>
-	);
+	return <ParticipantProfile id={'qwerty'} name={'Jimmy'} createdAt={'2024-04-29 16:44'} />;
 };
 
 export default ParticipantProfilePage;
